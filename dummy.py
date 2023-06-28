7ffcb05334212b53363713c1d35f39b68496d9a4
Lee, Jun Seok <lego3410@gmail.com>
2023-06-29 01:51:44 +0900

    Add DummyTensorDataset

---
+++ dummy.py
@@ -0,0 +1,23 @@
+from typing import Sequence
+
+import torch
+
+
+class DummyTensorDataset(torch.utils.data.IterableDataset):
+    def __init__(self, *example_tensors):
+        super().__init__()
+        self.generator = lambda: tuple(torch.randn_like(t) for t in example_tensors)
+
+    def __iter__(self):
+        while True:
+            yield self.generator()
+
+
+if __name__ == "__main__":
+    dataset = DummyTensorDataset(torch.zeros(1, 2, 3), torch.zeros(4, 5, 6))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=2)
+
+    iterator = iter(dataloader)
+
+    print(next(iterator)[0].shape)
+    print(next(iterator)[1].shape)
