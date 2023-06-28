53da343e52243e141456c5b2985f3b889ae53c6d
Lee, Jun Seok <lego3410@gmail.com>
2023-06-29 02:35:58 +0900

    Add tests to FileLR

--- file_lr.py
+++ file_lr.py
@@ -24,3 +24,37 @@ class FileLRCallback(Callback):
             config.interval = 'step'
             config.frequency = 1
             config.reduce_on_plateau = False
+
+
+if __name__ == "__main__":
+    import torch
+    import pytorch_lightning as pl
+    from dummy import DummyTensorDataModule
+
+    class Module(pl.LightningModule):
+        def __init__(self):
+            super().__init__()
+            self.param = torch.nn.Parameter(torch.zeros(1))
+
+        def training_step(self, batch, batch_idx):
+            (x,) = batch
+            loss = x.sum() + self.param
+            return loss
+
+        def configure_optimizers(self):
+            optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3)
+            scheduler = FileLR(optimizer, path="lr")
+            return [optimizer], [scheduler]
+
+        def on_before_optimizer_step(self, optimizer):
+            print(optimizer.param_groups[0]["lr"])
+
+    trainer = pl.Trainer(
+        callbacks=[FileLRCallback()],
+        max_steps=10,
+    )
+
+    trainer.fit(
+        model=Module(),
+        datamodule=DummyTensorDataModule(torch.zeros(3, 4, 5), batch_size=2),
+    )
