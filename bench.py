e6092a2ab6abe873d8aaa65fe0707660e2b95cf6
Lee, Jun Seok <lego3410@gmail.com>
2023-07-02 01:37:52 +0900

    Let bench() take optional `description` arg

--- bench.py
+++ bench.py
@@ -3,23 +3,26 @@ from time import time
 
 
 class bench:
+    def __init__(self, description=None):
+        self.description = description
+
     def __enter__(self):
         self.start = time()
         return self
 
     def __exit__(self, *args):
         duration = time() - self.start
-        print(f"bench: {duration:4f}s")
+        print(f"bench: '{self.description}' {duration:4f}s")
 
     def __call__(self, func=None):
-        name = func.__name__
+        description = self.description or func.__name__
 
         @wraps(func)
         def wrapper(*args, **kwargs):
             start = time()
             result = func(*args, **kwargs)
             duration = time() - start
-            print(f"bench: '{name}' {duration:4f}s")
+            print(f"bench: '{description}' {duration:4f}s")
 
             return result
 
@@ -27,7 +30,7 @@ class bench:
 
 
 if __name__ == "__main__":
-    with bench():
+    with bench('hello'):
         print("Hello world!")
 
     @bench()
